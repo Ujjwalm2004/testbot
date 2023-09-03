297da9771bdda7982a47617fd7fba31de164e89c
minato-king <143513435+minato-king@users.noreply.github.com>
2023-09-03 13:18:40 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
+
+bot = Client("test_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+@bot.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Hello, i am test bot!")
+
+
+# Start the bot
+bot.run()
